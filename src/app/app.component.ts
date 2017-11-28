6a8c1779656bdd5539ddee6486b34571417c96c1
Jan Koschke <jankoschke@googlemail.com>
2017-11-28 06:09:55 +0100

    - Added i18n - Added search page

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -4,15 +4,29 @@ import { StatusBar } from '@ionic-native/status-bar';
 import { SplashScreen } from '@ionic-native/splash-screen';
 
 import { TabsPage } from '../pages/tabs/tabs';
+import { TranslateService } from "@ngx-translate/core";
+import { Globalization } from "@ionic-native/globalization";
 
 @Component({
-  templateUrl: 'app.html'
-})
+             templateUrl : 'app.html'
+           })
 export class MyApp {
-  rootPage:any = TabsPage;
+  rootPage: any = TabsPage;
 
-  constructor(platform: Platform, statusBar: StatusBar, splashScreen: SplashScreen) {
+  constructor(platform: Platform, statusBar: StatusBar, splashScreen: SplashScreen, translate: TranslateService, globalization: Globalization) {
+    // this language will be used as a fallback when a translation isn't found in the current language
+    translate.setDefaultLang('en');
+
+    // the lang to use, if the lang isn't available, it will use the current loader to get them
+    //TODO Localize here
+    globalization.getPreferredLanguage().then((lang) => {
+      translate.use(lang.value);
+    }).catch(() => {
+      translate.use('en');
+    });
+
     platform.ready().then(() => {
+
       // Okay, so the platform is ready and our plugins are available.
       // Here you can do any higher level native things you might need.
       statusBar.styleDefault();
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,6 @@
-import { NgModule, ErrorHandler } from '@angular/core';
+import { ErrorHandler, NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-import { IonicApp, IonicModule, IonicErrorHandler } from 'ionic-angular';
+import { IonicApp, IonicErrorHandler, IonicModule } from 'ionic-angular';
 import { MyApp } from './app.component';
 
 import { AboutPage } from '../pages/about/about';
@@ -10,31 +10,52 @@ import { TabsPage } from '../pages/tabs/tabs';
 
 import { StatusBar } from '@ionic-native/status-bar';
 import { SplashScreen } from '@ionic-native/splash-screen';
+import { SearchPage } from "../pages/search/search";
+
+import { TranslateLoader, TranslateModule } from '@ngx-translate/core';
+import { TranslateHttpLoader } from "@ngx-translate/http-loader";
+import { HttpClient, HttpClientModule } from "@angular/common/http";
+import { Globalization } from "@ionic-native/globalization";
+
+export function createTranslateLoader(http: HttpClient) {
+  return new TranslateHttpLoader(http, './assets/i18n/', '.json');
+}
 
 @NgModule({
-  declarations: [
-    MyApp,
-    AboutPage,
-    ContactPage,
-    HomePage,
-    TabsPage
-  ],
-  imports: [
-    BrowserModule,
-    IonicModule.forRoot(MyApp)
-  ],
-  bootstrap: [IonicApp],
-  entryComponents: [
-    MyApp,
-    AboutPage,
-    ContactPage,
-    HomePage,
-    TabsPage
-  ],
-  providers: [
-    StatusBar,
-    SplashScreen,
-    {provide: ErrorHandler, useClass: IonicErrorHandler}
-  ]
-})
+            declarations : [
+              MyApp,
+              AboutPage,
+              ContactPage,
+              HomePage,
+              TabsPage,
+              SearchPage
+            ],
+            imports : [
+              BrowserModule,
+              HttpClientModule,
+              IonicModule.forRoot(MyApp),
+              TranslateModule.forRoot({
+                                        loader : {
+                                          provide : TranslateLoader,
+                                          useFactory : createTranslateLoader,
+                                          deps : [ HttpClient ]
+                                        }
+                                      })
+            ],
+            bootstrap : [ IonicApp ],
+            entryComponents : [
+              MyApp,
+              AboutPage,
+              ContactPage,
+              HomePage,
+              TabsPage,
+              SearchPage
+            ],
+            providers : [
+              StatusBar,
+              SplashScreen,
+              Globalization,
+              { provide : ErrorHandler, useClass : IonicErrorHandler }
+            ]
+          })
 export class AppModule {}
